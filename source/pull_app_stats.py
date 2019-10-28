8c41143542eec5cdcb1dc1fed85e19964d938c90
CheyenneNS <32466546+CheyenneNS@users.noreply.github.com>
2019-10-28 09:50:47 -0700

    Update pull_app_stats.py

--- source/pull_app_stats.py
+++ source/pull_app_stats.py
@@ -14,14 +14,14 @@ def get_app_stats(start_date, end_date):
 
     if type(start_date) == str:
         # Format date strings to datetime objects
-        start_date = datetime.datetime.strptime(start_date, '%m-%d-%Y')
-        start_date = datetime.datetime.combine(start_date, datetime.datetime.min.time())
-        end_date = datetime.datetime.strptime(end_date, '%m-%d-%Y')
-        end_date = datetime.datetime.combine(end_date, datetime.datetime.max.time())
+        start_date_dt = datetime.datetime.strptime(start_date, '%m-%d-%Y')
+        start_date_min = datetime.datetime.combine(start_date_dt, datetime.datetime.min.time())
+        end_date_dt = datetime.datetime.strptime(end_date, '%m-%d-%Y')
+        end_date_max = datetime.datetime.combine(end_date_dt, datetime.datetime.max.time())
 
         # datetime to epoch. Epoch format needed for elastic query
-        epoch_start = int(start_date.strftime('%s')) * 1000
-        epoch_end = int(end_date.strftime('%s')) * 1000
+        epoch_start = int(start_date_min.strftime('%s')) * 1000
+        epoch_end = int(end_date_max.strftime('%s')) * 1000
     else:
         # datetime to epoch. Epoch format needed for elastic query
         epoch_start = int(start_date.strftime('%s')) * 1000
@@ -31,7 +31,6 @@ def get_app_stats(start_date, end_date):
     job_states = metrics[0]['job_states']
 
     error_logs = []
-    #error_list = []
     for log in job_states:
         # Convert timestamps from milliseconds to seconds.
         millisec_crtime = log["creation_time"]/1000.0
@@ -41,18 +40,19 @@ def get_app_stats(start_date, end_date):
                 if log.get('status') == '':
                     errlog_dictionary = {"user" : log["user"], "error_msg": "_NULL_", "app_id" : log["app_id"], "type": "errorlogs",
                                  "job_id": log["job_id"], 'timestamp': creation_time_iso}
-                    #print(errlog_dictionary)
+                    c.to_logstashJson(errlog_dictionary)
+                    
                 else:
                     errlog_dictionary = {"user" : log["user"], "error_msg": log.get('status'), "app_id" : log["app_id"], "type": "errorlogs",
                                  "job_id": log["job_id"], 'timestamp': creation_time_iso}
+                    c.to_logstashJson(errlog_dictionary)
             
             
             else:
                 errlog_dictionary = {"user" : log["user"], "error_msg": log.get('status'), "app_id" : "None", "type": "errorlogs",
                                  "job_id": log["job_id"], 'timestamp': creation_time_iso}
-                print(errlog_dictionary)
+                c.to_logstashJson(errlog_dictionary)
             
-        c.to_logstashJson(errlog_dictionary)
             
             
     print("Error logs added to Logstash for date range: {} to {}".format(start_date, end_date))
