40d930cb47c69b79ae7eead70d7e5f7ed52ba6c7
Cheyenne Nelson <cnelson@Cheyennes-MBP.dhcp.lbl.gov>
2019-11-20 10:19:09 -0800

    Changed uploads_errorlogs to include date range.

--- source/upload_errorlogs.py
+++ source/upload_errorlogs.py
@@ -2,13 +2,33 @@
 #
 import pull_app_stats
 import time
+import sys
 import datetime
 print("############################################")
 print("App Stats Upload (UTC): " + str(datetime.datetime.utcnow()))
 start_time = time.time()
-# start_date = "01-10-2019""
-# end_date = "31-10-2019"
-# pull_app_stats.get_app_stats(start_date,end_date)
-pull_app_stats.get_app_stats()
-print("Uploading error logs to logstash stats took ",
-      time.time() - start_time, " seconds to run")
+
+if len(sys.argv) == 2:
+    start_date = sys.argv[0]
+    end_date = sys.argv[1]
+
+    try:
+        datetime.datetime.strptime(start_date, '%m-%d-%Y')
+    except ValueError:
+        raise ValueError("Incorrect start data format, should be MM-DD-YYYY")
+
+    try:
+        datetime.datetime.strptime(end_date, '%m-%d-%Y')
+    except ValueError:
+        raise ValueError("Incorrect end data format, should be MM-DD-YYYY")
+    pull_app_stats.get_app_stats(start_date, end_date)
+    print("Uploading error logs to logstash stats took ",
+          time.time() - start_time, " seconds to run")
+
+elif len(sys.argv) == 0:
+    pull_app_stats.get_app_stats()
+    print("Uploading error logs to logstash stats took ",
+          time.time() - start_time, " seconds to run")
+
+else:
+    print("Invalid number of arguments given")
