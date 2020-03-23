f591d2f929b04a00632d7fc5d69d349be5f92fe0
Cheyenne  Nelson <cnelson@docker03.berkeley.kbase.us>
2020-03-23 03:25:52 +0000

    adding necessary .sh files for EE2 methods and clients.

---
+++ bin/get_errored_apps_EE2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                    \
+                                                                                                
+
+python get_errored_apps_EE2.py
